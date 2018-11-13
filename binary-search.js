c8da90fa2b23d672be60d27f2568297866f24acc
Rob Merrill <robgmerrill@gmail.com>
2018-11-13 08:46:27 -0800

    Add comments to binary search

--- binary-search.js
+++ binary-search.js
@@ -1,21 +1,31 @@
+// Binary Search
 function binarySearch(arr, elem) {
+  	// define `start`
     var start = 0;
+  	// define `end`
     var end = arr.length - 1;
+  	// round down to define `middle`
     var middle = Math.floor((start + end) / 2);
+  	// while `middle` is not a match `elem` and `start`
+    // is less than or equal to `end`
     while(arr[middle] !== elem && start <= end) {
-        // console.log(start, middle, end);
-        if (elem < arr[middle]) {
-            end = middle - 1;
-        } else {
-            start = middle + 1;
-        }
-        middle = Math.floor((start + end) / 2);
+      if (elem < arr[middle]) {
+        // if `elem` is less than `middle` create new `end`
+          end = middle - 1;
+      } else {
+        // if `elem` is greater than `middle` create new `start`
+          start = middle + 1;
+      }
+      // based on new `start` or `end` redefine `middle`
+      middle = Math.floor((start + end) / 2);
     }
-    // console.log(start, middle, end);
+  	// if match, return `middle`
     if (arr[middle] === elem) {
       return middle;
     }
+  	// if no match, return `-1`
     return -1;
 }
-console.log(binarySearch([2,3,4,5,6,7], 15));
-console.log(binarySearch([2,3,4,5,6,7], 5));
+console.log(binarySearch([2,3,4,5,6,7], 15)); // -1
+console.log(binarySearch([2,3,4,5,6,7], 5)); // 3
+// find more at javascriptbootcamp.tech
